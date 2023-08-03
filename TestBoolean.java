58afc7b686dad0c7f7e18a8767add29963650e42
KyLin <kylin066@gmail.com>
2023-08-03 18:54:21 +0800

    feat：新增了字符集和Boolean型的学习代码

---
+++ TestBoolean.java
@@ -0,0 +1,9 @@
+public class TestBoolean {
+    // Java基本数据类型之Boolean型
+    public static void main(String[] args) {
+        boolean flag = true;
+        if(flag){
+            System.out.println("I love coding");
+        }
+    }
+}
---
+++ TestChar.java
@@ -0,0 +1,11 @@
+public class TestChar {
+    // Java基本数据类型之字符集
+    public static void main(String[] args) {
+        // 测试char
+        char c1='A';
+        char c2='陈';
+
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
