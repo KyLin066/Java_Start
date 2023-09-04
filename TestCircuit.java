68420b47e662992d60e440daeff7c66c460a2ca2
KyLin <kylin066@gmail.com>
2023-09-04 22:19:04 +0800

    feat：新增了Java循环结构和嵌套循环的学习代码

---
+++ TestCircuit.java
@@ -0,0 +1,33 @@
+public class TestCircuit {
+    // 测试循环结构
+    public static void main(String[] args) {
+        // 打印 1-100
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+
+        // 计算 1-100的和
+        int num = 0;
+        int sum = 0;
+        while (num <= 100) {
+            sum += num;
+            num++;
+        }
+        System.out.println("1-100的和：" + sum);
+
+        // 使用for循环打印 1-100
+        System.out.println("for循环打印1-100：");
+        for (int n = 0; n <= 100; n++) {
+            System.out.println(n);
+        }
+
+        // 使用for循环计算 1-100的和
+        int total = 0;
+        for (int m = 0; m <= 100; m++) {
+            total += m;
+        }
+        System.out.println("for循环计算1-100的和：" + total);
+    }
+}
---
+++ TestNestedCycle.java
@@ -0,0 +1,47 @@
+public class TestNestedCycle {
+    // 测试嵌套循环
+    public static void main(String[] args) {
+        // 打印输出十行，每行输出0-9
+        for (int line = 0; line < 10; line++) {
+            for (int num = 0; num < 10; num++) {
+                System.out.print(num + "\t");
+            }
+            System.out.println();
+        }
+
+        // 输出一个直角三角形
+        System.out.println("输出一个直角三角形：");
+
+        for (int line = 1; line <= 5; line++) {
+            for (int num = 1; num <= line; num++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+
+        // 输出一个倒立直角三角形
+        System.out.println("输出一个倒立直角三角形：");
+
+        for (int line = 1; line <= 5; line++) {
+            for (int num = 1; num <= 6 - line; num++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+
+        // 输出一个等腰三角形
+        System.out.println("输出一个等腰三角形：");
+        int tag = 4;
+        for (int line = 0; line < 5; line++) {
+            for (int num = 0; num < 10; num++) {
+                if (num >= tag - line && num <= tag + line) {
+                    System.out.print("* ");
+
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
